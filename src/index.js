d51995cd1dc218ae6d464ee6c8cd7ea19e2ab55a
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-02 11:40:59 +0200

    booklist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-function FirstComponent() {
-  return <h2>Hello World</h2>
-}
+import './index.css';
+const author = 'David Goggins';
+const title = "Can't Hurt Me";
+const img = './images/cant-hurt-me.jpg';
 
-// function FirstComponent() {
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h2', {}, 'Hello World')
-//   )
-// }
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book author={author} />
+      <Book title="random title" number={23} />
+    </section>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<FirstComponent />)
+const Book = () => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BookList />);
