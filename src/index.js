86bb7c47a0c408083382b0e05d42a21d7c7d74ce
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-03 11:46:43 +0200

    props, key

--- src/index.js
+++ src/index.js
@@ -2,21 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-const author = 'David Goggins'; // Props
-const title = "Can't Hurt Me"; // Props
-const img = './images/cant-hurt-me.jpg'; // Props
+
+const books = [
+  {
+    author: 'Tim Grover',
+    title: 'Winning',
+    img: 'https://m.media-amazon.com/images/I/61dJJaKOeOL._AC_UL400_.jpg',
+    id: 1,
+  },
+
+  {
+    author: 'Robert Green',
+    title: 'Mastery',
+    img: 'https://m.media-amazon.com/images/I/61kZgW7wemL._AC_UL400_.jpg',
+    id: 2,
+  },
+
+  {
+    author: 'David Goggins',
+    title: "Can't Hurt Me",
+    img: 'https://m.media-amazon.com/images/I/61pDNU9qEGL._AC_UL400_.jpg',
+    id: 3,
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={author} />
-      <Book title="random title" number={23} />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 };
 
-// Props
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props;
+  console.log(props);
   return (
     <article className="book">
       <img src={img} alt={title} />
