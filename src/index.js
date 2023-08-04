b94d43922d11c0f1965f0e86d94584a4858e5b72
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-04 12:00:17 +0200

    props drilling, arrow function

--- src/index.js
+++ src/index.js
@@ -7,42 +7,47 @@ const books = [
   {
     author: 'Tim Grover',
     title: 'Winning',
-    img: 'https://m.media-amazon.com/images/I/61dJJaKOeOL._AC_UL400_.jpg',
+    img: 'https://timgrover.com/wp-content/uploads/2021/07/NYT-winning-1.png',
     id: 1,
   },
 
   {
-    author: 'Robert Green',
-    title: 'Mastery',
-    img: 'https://m.media-amazon.com/images/I/61kZgW7wemL._AC_UL400_.jpg',
+    author: 'Cal Newport',
+    title: "So Good They Can't Ignore You",
+    img: 'https://i.ebayimg.com/images/g/OuAAAOSwuRJcrfN8/s-l500.jpg',
     id: 2,
   },
 
   {
     author: 'David Goggins',
     title: "Can't Hurt Me",
-    img: 'https://m.media-amazon.com/images/I/61pDNU9qEGL._AC_UL400_.jpg',
+    img: 'https://davidgoggins.com/wp-content/uploads/2022/10/cant-hurt-me-group.jpg',
     id: 3,
   },
 ];
 
 const BookList = () => {
+  const someValue = 'Value returned';
+  const displayValue = () => {
+    console.log(someValue);
+  };
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} displayValue={displayValue} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author } = props;
-  console.log(props);
+  const { img, title, author, displayValue } = props;
+
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayValue}>Click me</button>
       <h4>{author}</h4>
     </article>
   );
