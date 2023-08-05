ec2c81f749a57c27201775ec2f3a70510e6c466d
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-05 10:23:23 +0200

    es6 modules

---
+++ src/books.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+    author: 'Tim Grover',
+    title: 'Relentless',
+    img: 'https://impacttheory.com/wp-content/uploads/2017/09/tim-grover-relentless.jpg',
+    id: 1,
+  },
+
+  {
+    author: 'Robert Greene',
+    title: 'Mastery',
+    img: 'https://impacttheory.com/wp-content/uploads/2017/09/robert-greene-mastery.jpg',
+    id: 2,
+  },
+
+  {
+    author: 'David Goggins',
+    title: "Can't Hurt Me",
+    img: 'https://impacttheory.com/wp-content/uploads/2019/01/DavidGoggins.png',
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,54 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-
-const books = [
-  {
-    author: 'Tim Grover',
-    title: 'Winning',
-    img: 'https://timgrover.com/wp-content/uploads/2021/07/NYT-winning-1.png',
-    id: 1,
-  },
-
-  {
-    author: 'Cal Newport',
-    title: "So Good They Can't Ignore You",
-    img: 'https://i.ebayimg.com/images/g/OuAAAOSwuRJcrfN8/s-l500.jpg',
-    id: 2,
-  },
-
-  {
-    author: 'David Goggins',
-    title: "Can't Hurt Me",
-    img: 'https://davidgoggins.com/wp-content/uploads/2022/10/cant-hurt-me-group.jpg',
-    id: 3,
-  },
-];
+import { books } from './books.js';
+import Book from './Book.js';
 
 const BookList = () => {
-  const someValue = 'Value returned';
-  const displayValue = () => {
-    console.log(someValue);
-  };
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} displayValue={displayValue} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author, displayValue } = props;
-
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={displayValue}>Click me</button>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
